94f961a11e78b05fa92f7a657f9650d77e2b5f7f
Tintin Ho <holoktin97@gmail.com>
2016-12-06 14:09:33 +0800

    basic feature

---
+++ test.js
@@ -0,0 +1,83 @@
+
+var vstructs = require('.');
+
+var expect = require('chai').expect;
+var assert = require('assert');
+
+describe('vstructs', function () {
+
+    var Temperature = vstructs('Celsius', 'Fahrenheit');
+    var Foo = vstructs(['A', 'B', 'C', 'D']);
+
+    it('example #1', function () {
+
+        Temperature.impl({
+            convert: function () {
+                return this.match({
+                    Celsius: function (c) {
+                        return Temperature.Fahrenheit(c * 9/5 + 32);
+                    },
+                    Fahrenheit: function (f) {
+                        return Temperature.Celsius((f - 32) * 5/9);
+                    },
+                });
+            }
+        });
+
+        var c = Temperature.Celsius(30);
+        var f = c.convert();
+
+        expect(c.variant).to.be.equal('Celsius');
+        expect(f.variant).to.be.equal('Fahrenheit');
+
+        expect(c[0]).to.be.equal(30);
+        expect(f[0]).to.be.equal(86);
+
+        assert(Temperature.Celsius(10).equals(Temperature.Celsius(10)));
+        assert(Temperature.Celsius(10).notEquals(Temperature.Celsius(15)));
+        assert(Temperature.Celsius(10).notEquals(Temperature.Fahrenheit(15)));
+
+        var temp = Temperature.Celsius(123);
+        assert(temp.clone().equals(temp));
+    });
+
+    it('example #2', function () {
+        var Numbers = vstructs(['N1', 'N3', 'N5', 'N6']);
+
+        // find the reminder of 3
+        var reminder3 = function (e) {
+            return e.match({
+                N1: 1,
+                N5: 2,
+                _: 0, // _ indicates default case
+            });
+        };
+
+        expect(reminder3(Numbers.N1())).to.be.equal(1);
+        expect(reminder3(Numbers.N3())).to.be.equal(0);
+        expect(reminder3(Numbers.N5())).to.be.equal(2);
+        expect(reminder3(Numbers.N6())).to.be.equal(0);
+
+        assert(Numbers.N1().typeOf('N1'));
+        assert(Numbers.N3().oneOf('N3', 'N5'));
+        assert(Numbers.N5().oneOf(['N5', 'N6']));
+    });
+
+    it('caseOf #1', function () {
+        var result = Foo.A(10).caseOf('A', function (i) {
+            return i + 1;
+        });
+
+        expect(result).to.be.equal(11);
+    });
+
+
+    it('caseOf #2', function () {
+        var result = Foo.B(10).caseOf('A', function (i) {
+            return i + 1;
+        });
+
+        /*jshint expr: true*/
+        expect(result).to.be.undefined;
+    });
+});
---
+++ vstructs.js
@@ -0,0 +1,112 @@
+
+var verifyName = function (variant) {
+    if (!(typeof variant === 'string' && /[A-Z][a-zA-Z0-9]*/.test(variant))) {
+        throw new Error('invalid variant name ' + variant);
+    }
+};
+
+var collectArgs = function (args) {
+    if (Array.isArray(args[0])) {
+        return args[0];
+    } else {
+        return [].slice.call(args);
+    }
+};
+
+var VirtualStructs = module.exports = function (variants) {
+    var variantNames = collectArgs(arguments);
+    var that = Object.create(VirtualStructs.prototype);
+    that.variants = {};
+
+    variantNames.forEach(verifyName);
+    variantNames.forEach(function (name) {
+        that.extend(name);
+    });
+
+    return that;
+};
+
+VirtualStructs.prototype.extend = function (variant) {
+    verifyName(variant);
+    this.variants[variant] = newVariant(variant);
+    if (!this[variant]) {
+        this[variant] = this.variants[variant];
+    }
+    return this;
+};
+
+VirtualStructs.prototype.impl = function (methods) {
+    Object.keys(this.variants).forEach(function (variantName) {
+        var variantConstructor = this.variants[variantName];
+        Object.keys(methods).forEach(function (methodName) {
+            var method = methods[methodName];
+            variantConstructor.prototype[methodName] = method;
+        });
+    }, this);
+    return this;
+};
+
+var variantPrototype = Object.create(Array.prototype);
+
+variantPrototype.typeOf = function (v) {
+    return v === this.variant;
+};
+
+variantPrototype.match = function (dispatcher) {
+    var handler = dispatcher[this.variant] || dispatcher._;
+    if (typeof handler === 'function') {
+        return handler.apply(handler, this);
+    } else {
+        return handler;
+    }
+};
+
+variantPrototype.caseOf = function (variant, handler) {
+    var dispatcher = {};
+    dispatcher[variant] = handler;
+    return this.match(dispatcher);
+};
+
+variantPrototype.equals = function (other) {
+    return other.variant === other.variant && this.every(function (v, i) {
+        return v === other[i];
+    });
+};
+
+variantPrototype.notEquals = function (val) {
+    return !this.equals(val);
+};
+
+variantPrototype.clone = function () {
+    return this.constructor.apply(this.slice());
+};
+
+variantPrototype.oneOf = function () {
+    var variantNames = collectArgs(arguments);
+    return variantNames.some(function (name) {
+        return this.typeOf(name);
+    }, this);
+};
+
+variantPrototype.toString = function () {
+    return this.variant + '(' + this.join(', ') + ')';
+};
+
+variantPrototype.valueOf = function () {
+    return this.variant;
+};
+
+var newVariant = function (variant) {
+    var ConcreteVariant = function () {
+        var that = Object.create(ConcreteVariant.prototype);
+        that.variant = variant;
+        [].push.apply(that, [].slice.call(arguments));
+
+        return that;
+    };
+
+    ConcreteVariant.prototype = Object.create(variantPrototype);
+    ConcreteVariant.prototype.constructor = ConcreteVariant;
+
+    return ConcreteVariant;
+};
